dce7bc50967f71f4b27ef7d03c7c4dcafc64afef
Toothless <hyunjinney@naver.com>
2025-03-18 17:45:33 +0900

    fix. 강사님 코드보고 수정
    - client 출력이 잘 안돼서 다시 수정해야 할 듯

--- Server/Client/Program.cs
+++ Server/Client/Program.cs
@@ -15,50 +15,36 @@ namespace Client
         {
             string jsonString = "{\"message\" : \"안녕하세요\"}";
             byte[] messsage = Encoding.UTF8.GetBytes(jsonString);
-            ushort length = (ushort)messsage.Length; //메세지 전체 길이 저장 
+            ushort length = ((ushort)messsage.Length); //메세지 전체 길이 저장 
 
             //길이 넣을 두 개짜리 버퍼 
             byte[] lengthBuffer = new byte[2];
-            lengthBuffer = BitConverter.GetBytes(length);
+            lengthBuffer = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)length));
 
             byte[] buffer = new byte[2 + length]; //앞에 2바이트 + 진짜 문자열의 길이
 
             Buffer.BlockCopy(lengthBuffer, 0, buffer, 0, 2);
             Buffer.BlockCopy(messsage, 0, buffer, 2, length);
 
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Loopback, 4000);
-            serverSocket.Connect(clientEndPoint);
+            clientSocket.Connect(clientEndPoint);
 
-            int sendLength = serverSocket.Send(buffer, buffer.Length, SocketFlags.None);
+            int sendLength = clientSocket.Send(buffer, buffer.Length, SocketFlags.None);
             //받아올 때도 앞에 두 개 받아와야 함 
             //원래 만약 1바이트만 받으면 무조건 2개 받으라고 while문 받으라고 처리해줘야 함
-            int recvLength = serverSocket.Receive(lengthBuffer, 2, SocketFlags.None);
+            int recvLength = clientSocket.Receive(lengthBuffer, 2, SocketFlags.None);
             length = BitConverter.ToUInt16(lengthBuffer, 0); // 길이 정보를 받아와야 함
+            length = (ushort)IPAddress.NetworkToHostOrder((ushort)length); 
 
             byte[] receiveBuffer = new byte[4096];
-            recvLength = serverSocket.Receive(receiveBuffer, length, SocketFlags.None);
+            recvLength = clientSocket.Receive(receiveBuffer, length, SocketFlags.None);
 
             string jsonStr = Encoding.UTF8.GetString(receiveBuffer);
             Console.WriteLine(jsonStr);
 
-            serverSocket.Close();
+            clientSocket.Close();
 
-            /*            int sendLength = serverSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
-                        while (sendLength < buffer.Length)
-                        {
-                            int SendLength = serverSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
-                        }
-
-                        byte[] buffer2 = new byte[1024];
-
-                        int recvLength = serverSocket.Receive(buffer2);
-
-                        String str = Encoding.UTF8.GetString(buffer2);
-                        Console.WriteLine(str);
-
-                        serverSocket.Close();
-            */
         }
     }
 }
--- Server/L250318/Program.cs
+++ Server/L250318/Program.cs
@@ -29,7 +29,8 @@ namespace L250318
             byte[] headerBuffer = new byte[2]; //바이트 수는 선택사항, 원하는 위치 조정은 ArraySegment 사용 C#은 포인터가 없어서
             //원래 여기에 길이수 맞는지 예외처리 추가해줘야 함
             int recvLength = clientSocket.Receive(headerBuffer, 2, SocketFlags.None);
-            ushort packetLength = BitConverter.ToUInt16(headerBuffer, 0);
+            short packetLength = BitConverter.ToInt16(headerBuffer, 0);
+            packetLength = IPAddress.NetworkToHostOrder(packetLength);   
 
             //실제 패킷 (header 길이 만큼)
             byte[] dataBuffer = new byte[4096];
@@ -44,7 +45,8 @@ namespace L250318
             //다시 전송 메세지
             string message = "{\"message\" : \"잘 받았옹\"}";
             byte[] messsageBuffer = Encoding.UTF8.GetBytes(message);
-            ushort length = (ushort)messsageBuffer.Length; //메세지 전체 길이 저장 
+            //ushort length = (ushort)messsageBuffer.Length; //메세지 전체 길이 저장 
+            ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messsageBuffer.Length);
 
             //전체 길이 자료는
             headerBuffer = BitConverter.GetBytes(length);
@@ -52,11 +54,12 @@ namespace L250318
             byte[] packetBuffer = new byte[headerBuffer.Length + messsageBuffer.Length]; //앞에 2바이트 + 진짜 문자열의 길이
 
             Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
-            Buffer.BlockCopy(messsageBuffer, 0, packetBuffer, headerBuffer.Length, length);
+            Buffer.BlockCopy(messsageBuffer, 0, packetBuffer, headerBuffer.Length, messsageBuffer.Length);
 
             int sendLength = clientSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
 
-
+            clientSocket.Close();
+            listenSocket.Close();
             
         }
     }
