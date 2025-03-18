cc061e7e04339fdee2ef315f10fd32087e9d33db
Toothless <hyunjinney@naver.com>
2025-03-18 10:31:31 +0900

    [250318] 파일 전송 실습

---
+++ L250318_Practice2/Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Sample
+    {
+        public string imgStr;
+
+        public Sample(string imgStr)
+        {
+            this.imgStr = imgStr;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Loopback, 4000);
+
+            serverSocket.Connect(clientEndPoint);
+
+            byte[] buffer = new byte[1024];
+            int recevLength =serverSocket.Receive(buffer);
+        
+
+            string jsonString = Encoding.UTF8.GetString(buffer);
+
+            Sample receiveSample = JsonConvert.DeserializeObject<Sample>(jsonString);
+            byte[] imageBytes = Convert.FromBase64String(receiveSample.imgStr);
+
+            File.WriteAllBytes("receiveImg.webp", imageBytes);
+            Console.WriteLine(receiveSample.imgStr);
+        }
+    }
+}
---
+++ L250318_Practice2/Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        class Sample
+        {
+            public string imgStr;
+
+            public Sample(string imgStr)
+            {
+                this.imgStr = imgStr;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Socket listenSocket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint listenEndPoiint = new IPEndPoint(IPAddress.Any, 4000);
+
+            listenSocket.Bind (listenEndPoiint);
+            listenSocket.Listen(10);
+
+            bool isRunning = true;
+            while(isRunning)
+            {
+                Socket clientSocket = listenSocket.Accept ();
+
+                string baseImage = Convert.ToBase64String(File.ReadAllBytes("./1.webp"));
+                Console.WriteLine(baseImage);
+
+                byte[] buffer = new byte[1024];
+
+                Sample sample = new Sample(baseImage);
+                string jsonData = JsonConvert.SerializeObject(sample);
+                byte[] buffer2 = Encoding.UTF8.GetBytes(jsonData);
+
+                int sendLength = clientSocket.Send(buffer2);
+
+                clientSocket.Close();
+            }
+        }
+    }
+}
